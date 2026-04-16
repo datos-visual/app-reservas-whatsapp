12ae64075ef810d4f056be8a46bda9a0e2ebca22
José Manuel <josemanuel.pina@rtve.es>
2026-04-16 14:16:52 +0200

    Corrección en Registration

--- frontend/src/components/RegistrationForm.tsx
+++ frontend/src/components/RegistrationForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, FormEvent, ChangeEvent, Fragment } from "react";
+import { useState, FormEvent, ChangeEvent } from "react";
 import Link from "next/link";
 import { AlertCircle, CheckCircle } from "lucide-react";
 
